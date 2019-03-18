5b8bb910cce7641da5d603045a09e5396d0c88d5
Panteraetec <panterahas@gmail.com>
2019-03-18 18:39:55 -0300

    Tela nova de Cadastro de Perfil

---
+++ app/src/main/java/com/crowtech/petzdaily/cadastro_perfil.java
@@ -0,0 +1,13 @@
+package com.crowtech.petzdaily;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cadastro_perfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_perfil);
+    }
+}
